d2ad2dd6213753dac258dc1c455014f85f7f740a
박성경 <sungkyong_park@bagseong-gyeong-ui-MacBookPro.local>
2020-03-03 16:56:07 +0900

    init project

---
+++ src/test/java/com/gitflow/example/gitflow/GitflowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitflow.example.gitflow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitflowApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
